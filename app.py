3526443b68c5e006eb5b16f5670465370a9f3d78
Alan Zheng <azheng106@gmail.com>
2024-02-06 23:43:27 -0500

    Rudimentary calculation system for n-qubits, not sure if it's correct

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-from util import infix_to_postfix, eval_postfix
-from State import State
+from util.evalutil import infix_to_postfix, eval_postfix
+from util.state import State
+from util.qubitutil import *
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def index():
 
 
 @app.route('/calculate/3qubit', methods=['POST'])
-def calculate():
+def calculate_slocc():
     try:
         """ Calculate and print the SLOCC class on the website when the user inputs 8 constants and presses 'submit' """
         data = request.json  # data (dictionary): {'c0': 'userinput', 'c1': 'userinput', ... , 'c7': 'userinput'}
@@ -37,6 +38,29 @@ def calculate():
         return jsonify({'classification': 'Belongs to ' + classification + ' SLOCC class'})
     except Exception as e:
         return jsonify({'error': str(e)}), 400  # Error code 400
+
+
+@app.route('/calculate/nQubit', methods=['POST'])
+def calculate_entanglement():
+    try:
+        data = request.json  # Ex. of data: {'n': '4', 'pairs': {'3': '123', '2': '13', '4': '12', '': ''}}
+        num_qubits = data['n']
+
+        pairs = data['pairs']
+        pairs.popitem()  # Remove the last entry in dictionary, which is an empty pair ('': '')
+        for key in pairs:  # Blank value entries default to 0
+            if pairs[key] == '':
+                pairs[key] = '0'
+        qubit_value_pairs = {convert_to_basic_state(int(num_qubits), int(key)): eval_postfix(infix_to_postfix(value)) for key, value in pairs.items()}
+
+        #print(f"Original state: {qubit_value_pairs}")
+        classification = 'State is unknown'
+        if is_entangled(qubit_value_pairs, int(num_qubits)):
+            classification = 'State is entangled'
+
+        return jsonify({'classification': classification})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 
 if __name__ == '__main__':
---
+++ util/qubitutil.py
@@ -0,0 +1,48 @@
+def convert_to_basic_state(num_qubits, constant_number) -> str:
+    """
+    Convert a constant number to its basic state (binary) representation. Ex. C3 is |011>
+    """
+    num_qubits = int(num_qubits)  # Convert string to int
+    if constant_number > (2 ** num_qubits - 1):
+        raise Exception
+    binary = bin(constant_number).replace('0b', '')
+    while len(binary) < num_qubits:
+        binary = '0' + binary
+    return binary
+
+
+def remove_qubit_n(n, pairs_dictionary) -> dict:
+    """
+    Remove qubit n from given state
+    :param n: Qubit number to remove
+    :param pairs_dictionary: Constant + basic state pairs that make up the state
+    :return: Updated dictionary with qubit n removed
+    """
+    return {(key[:n] + key[n + 1:]): value for key, value in pairs_dictionary.items()}
+
+
+def is_entangled_2qubit(pairs_dictionary) -> bool:
+    if len(pairs_dictionary) > 4:
+        raise ValueError('Not a 2 qubit state')
+    c0 = pairs_dictionary.get('00', 0)  # Default to 0 if not found
+    c1 = pairs_dictionary.get('01', 0)
+    c2 = pairs_dictionary.get('10', 0)
+    c3 = pairs_dictionary.get('11', 0)
+    return c0 * c3 != c1 * c2
+
+
+def is_entangled(pairs_dictionary, n) -> bool:
+    #print(f"Checking {n}-qubit state: {pairs_dictionary}")
+    if n == 2:
+        return is_entangled_2qubit(pairs_dictionary)
+    else:
+        entangled_count = 0
+        for i in range(n):
+            qubit_i_removed = remove_qubit_n(i, pairs_dictionary)
+            #print(f"Qubit {i} removed: {qubit_i_removed}. Count = {entangled_count}")
+            if is_entangled(qubit_i_removed, n - 1):
+                entangled_count += 1
+                #print(f"State {qubit_i_removed} is entangled. Count = {entangled_count}")
+            if entangled_count >= 2:
+                return True
+    return False
